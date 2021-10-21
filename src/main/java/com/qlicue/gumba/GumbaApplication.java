82434d99f606cd42ed5b67ec9dca8d20101f668c
admin <kalrobbynson@gmail.com>
2021-10-21 10:51:59 +0300

    Initial commit

---
+++ src/main/java/com/qlicue/gumba/GumbaApplication.java
@@ -0,0 +1,13 @@
+package com.qlicue.gumba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GumbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GumbaApplication.class, args);
+	}
+
+}
